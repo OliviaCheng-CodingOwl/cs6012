2b24932d98386968f01f3d436f01a2763cca8c78
Manman Cheng <olivia.chengman@gmial.com>
2022-11-08 18:22:52 -0700

    add more tests

--- learnTest/src/lab01/DiffUtil.java
+++ learnTest/src/lab01/DiffUtil.java
@@ -6,11 +6,11 @@ public class DiffUtil {
             return -1;
         }
 
-        int diff = a[0] - a[1];
+        int diff = Math.abs(a[0] - a[1]);
 
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
-                int tmp_diff = a[i] - a[j];
+                int tmp_diff = Math.abs(a[i] - a[j]);
 
                 if (tmp_diff < diff)
                     diff = tmp_diff;
--- learnTest/src/lab01/DiffUtilTest.java
+++ learnTest/src/lab01/DiffUtilTest.java
@@ -7,17 +7,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiffUtilTest {
-    private int[] arr1, arr2, arr3;
+    private int[] arr1, arr2, arr3,arr4,arr5;
 
     @BeforeEach
     protected void setUp() throws Exception {
         arr1 = new int[0];
         arr2 = new int[] { 3, 3, 3 };
         arr3 = new int[] { 52, 4, -8, 0, -17 };
+        arr4 = new int[] { -4,-1,2,5,8 };
+        arr5 = new int[] { -4,-1,2,3,4 };
     }
 
     @AfterEach
     void tearDown() {
+        arr1 = null;
+        arr2 = null;
+        arr3 = null;
+        arr4 = null;
+        arr5 = null;
     }
 
     @Test
@@ -25,6 +32,8 @@ class DiffUtilTest {
         emptyArray();
         allArrayElementsEqual();
         smallRandomArrayElements();
+        allSameDiff();
+        multiSmallestDiff();
     }
     public void emptyArray() {
         assertEquals(-1, DiffUtil.findSmallestDiff(arr1));
@@ -37,4 +46,13 @@ class DiffUtilTest {
     public void smallRandomArrayElements() {
         assertEquals(4, DiffUtil.findSmallestDiff(arr3));
     }
+
+    public void allSameDiff() {
+        assertEquals(3, DiffUtil.findSmallestDiff(arr4));
+    }
+
+    public void multiSmallestDiff() {
+        assertEquals(1, DiffUtil.findSmallestDiff(arr5 ));
+    }
+
 }
